fe6d89b396359e49251ae3683b9b9ba30cbebe64
Urs Grupp <urs@21sieben.de>
2021-06-07 16:16:49 +0200

    feat: integrate CI colors and webfonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -7,9 +9,32 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontWeight: {
+      light: 300,
+      normal: 400,
+    },
     extend: {
       fontFamily: {
-        sans: ["Roboto", "Arial", "sans-serif"],
+        sans: ["neue-haas-grotesk-display", "sans-serif"],
+        mono: [
+          "basier_square_mono",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+      colors: {
+        warmGray: {
+          600: "#4B4A46",
+        },
+        red: {
+          600: "#E63209",
+        },
       },
     },
   },
